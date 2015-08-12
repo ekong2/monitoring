6d88994b43b330bb631d479918f188ccbb848c0a
Eddie Kong <eddiek93@gmail.com>
2015-08-12 00:01:58 -0700

    continuous updates every 2 seconds

--- views/js/app.js
+++ views/js/app.js
@@ -1,32 +1,44 @@
 angular.module("main", [])
-         .controller("info", function($scope, $http) {
-            $scope.disk = {};
-            $scope.processes = {};
-            $scope.tasks = {};
+ .controller("info", function($scope, $http, $timeout) {
+    $scope.disk = {};
+    $scope.processes = {};
+    $scope.tasks = {};
 
-            $http.get('/disk').
-			  then(function(response) {
-			    $scope.disk = response.data;
-			  }, function(response) {
-			  	//Error from the server
-			    console.log(response);
-			  });
+    $scope.getData = function(){
+	    $http.get('/disk').
+		  then(function(response) {
+		    $scope.disk = response.data;
+		  }, function(response) {
+		  	//Error from the server
+		    console.log(response);
+		  });
 
-			$http.get('/processes').
-			  then(function(response) {
-			  	$scope.processes = response.data;
-			  }, function(response) {
-			  	//Error from the server
-			    console.log(response);
-			  });
+		$http.get('/processes').
+		  then(function(response) {
+		  	$scope.processes = response.data;
+		  }, function(response) {
+		  	//Error from the server
+		    console.log(response);
+		  });
 
-			$http.get('/tasks').
-			  then(function(response) {
-			  	$scope.tasks = response.data;
-			  	console.log($scope.tasks);
-			  }, function(response) {
-			  	//Error from the server
-			    console.log(response);
-			  });
+		$http.get('/tasks').
+		  then(function(response) {
+		  	$scope.tasks = response.data;
+		  	console.log($scope.tasks);
+		  }, function(response) {
+		  	//Error from the server
+		    console.log(response);
+		  });
+    }
 
-         });
+    //Repeatedly fetch data every 2 seconds
+    $scope.fetchData = function(){
+    	$timeout(function(){
+    		$scope.getData();
+    		$scope.fetchData();
+    	}, 2000);
+    }
+
+    $scope.fetchData();
+
+ });
